dd421cab6e39ce243b5b72eeb03dd1827a81bbd0
Kevin Kim <kevink1103@gmail.com>
2019-03-05 18:30:13 +0800

    feat: familiarization with TS

---
+++ index.js
@@ -0,0 +1,9 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const name = "Kevin", age = 25, gender = "male";
+// Optional type available in TypeScript
+const sayHi = (name, age, gender) => {
+    return `Hello ${name}, you are ${age} years old and you are a ${gender}`;
+};
+console.log(sayHi(name, age, gender));
+//# sourceMappingURL=index.js.map
